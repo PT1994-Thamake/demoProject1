e8ab1319edfec3e71170f219da35ad0f1e3fa594
pratik <pratik94qa@gmail.com>
2024-12-06 22:21:32 +0530

    new changes

--- src/test/java/com/cicd/testdemo/AutomationTalkDemo.java
+++ src/test/java/com/cicd/testdemo/AutomationTalkDemo.java
@@ -2,23 +2,19 @@ package com.cicd.testdemo;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AutomationTalkDemo {
 	public static WebDriver driver;
 
 	@BeforeMethod
        public void setUp() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
+		//WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();  
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
@@ -38,11 +34,8 @@ public class AutomationTalkDemo {
     	@Test
     	public void Test3() {
     		driver.navigate().to("https://www.AutomationTalks.com");
-<<<<<<< HEAD
     		System.out.println("NewTest3 title is" + driver.getTitle());
-=======
-    		System.out.println("title is" + driver.getTitle());
->>>>>>> e864103d2431c0b5eaa02809dc1bb5c3692f6363
+
     	}
     	
     	@AfterMethod
